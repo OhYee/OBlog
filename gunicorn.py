690d43ab45fe40f26a6a54b6489eda153681a154
OhYee <oyohyee@oyohyee.com>
2018-02-19 23:29:03 +0800

    gunicorn

---
+++ gunicorn.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(0,'/data/wwwroot/default/OBlog')
+
+def Log(text, *args):
+    logStr = (text + '\n') % args
+    with open(r'/data/wwwroot/default/OBlog/log/test.log', 'a') as f:
+        f.write(logStr)
+
+Log("run wsgi 1")
+
+from OBlog import app as application
+from werkzeug.contrib.fixers import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app)
+app.run(threaded=True)
+
+Log("run wsgi 2")
