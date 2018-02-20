27fa35f40ce19325e2eac2ad244ba86139d2db1a
OhYee <oyohyee@oyohyee.com>
2018-02-20 15:27:02 +0800

    删除测试log

--- OBlog/back/initSite.py
+++ OBlog/back/initSite.py
@@ -1,28 +1,4 @@
 from flask import g
 from .getData import Site
-from .sendemail import thirdSMTP
-import jieba
-
-
 import sys
 import os
-
-def Log(text, *args):
-    logStr = (text + '\n') % args
-    with open(os.path.join(sys.path[0], r'log/test.log'), 'a+') as f:
-        f.write(logStr)
-
-
-def initSite():
-    print("OBlog start")
-    Log("run initSite")
-    
-    #jieba.initialize()
-    # app.app_context().push()  
-    # current_app.name  
-    # g.test="test"
-    # g.Site = Site.getSite()
-    # if g.Site["config"]["smtp"] == '1':
-    #     g.smtp = thirdSMTP(g.Site["config"]["smtpservice"], g.Site["config"]["smtpuser"],
-    #                        g.Site["config"]["smtppassword"], g.Site["config"]["smtpport"])
-    # # g.db.close()
--- wsgi.py
+++ wsgi.py
@@ -1,18 +1,9 @@
 import sys
 sys.path.insert(0,'/data/wwwroot/default/OBlog')
 
-def Log(text, *args):
-    logStr = (text + '\n') % args
-    with open(r'/data/wwwroot/default/OBlog/log/test.log', 'a') as f:
-        f.write(logStr)
-
-Log("run wsgi 1")
-
 from OBlog import app
 from werkzeug.contrib.fixers import ProxyFix
 
 if __name__=='__main__':
     app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(debug=False,threaded=True)
-
-Log("run wsgi 2")
