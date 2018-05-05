2035bde6f106e3ca4ca62c74471ce943cd15b5af
OhYee <oyohyee@oyohyee.com>
2018-05-05 23:55:48 +0800

    goods

--- OBlog/blueprint/goods/__init__.py
+++ OBlog/blueprint/goods/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 goodsApiBP = Blueprint('goodsApiBlueprint', __name__)
 goodsAdminBP = Blueprint('goodsAdminBlueprint', __name__)
 
-from . import admin, api,view
+from . import admin, api
