51bff3f1d4732397fcd0af0a02a7fdb98b8a8fbc
OhYee <oyohyee@oyohyee.com>
2018-12-04 13:47:11 +0800

    update:修改字体以及测试端口号

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
     args = sys.argv
     if len(sys.argv) == 1:
-        app.run(debug=True, threaded=True)
+        app.run(debug=True, threaded=True, host='0.0.0.0', port=8000)
     else:
         import importlib
         importlib.import_module('tools.'+args[1])
